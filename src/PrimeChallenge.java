efa12acc03ae54e6f4371b6cd5e923474a977094
Someone else <random@email.com>
2018-09-21 16:51:06 -0400

    Initial commit

---
+++ src/PrimeChallenge.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class PrimeChallenge {
+    public static void main(String[] args) {
+        int max = getMaxNum(int););
+        printPrimeChallenge(int);
+        //invoke method
+    }
+    //use method
+        private static void getMaxNum( ) {
+            Scanner in = new Scanner(System.in);
+            int max = 0;
+
+            System.out.printf("Enter number to print to: ");
+            max = in.nextInt();
+
+            return max;
+
+
+            //invoke  method
+            private static void printPrimeChallenge(max){
+                String output;
+
+                for (int i = 1; i <= max; i++) {
+                    output = "";
+
+
+                    if (i % 2 != 0) {
+                        System.print.outln();
+                    }
+                        
+                        if (i % 3 != 0)
+                            if (i % 4 != 0)
+                                if (i % 5 != 0)
+                                    System.out.println("prime");
+
+
+                    }
+                }
+            }
+        }
